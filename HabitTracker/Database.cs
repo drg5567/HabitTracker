460579f725baa3aa30bd195bb8273daebb848701
drg5567 <drg5567@rit.edu>
2025-03-24 12:52:52 -0400

    Initial Setup

---
+++ HabitTracker/Database.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace HabitTracker
+{
+    class Database
+    {
+        private string db_name;
+
+        public Database(string db_name)
+        {
+            this.db_name = db_name;
+            createDBIfNonExistant();
+        }
+
+        public void createDBIfNonExistant()
+        {
+            using (var connection = new SqliteConnection("Data Source=" + this.db_name))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = "";
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (SqliteException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    command.Dispose();
+                    connection.Dispose();
+                }
+            }
+        }
+    }
+}
--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
+
+namespace HabitTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
