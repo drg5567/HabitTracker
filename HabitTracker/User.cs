44b1f72f01bc70c3519f981f4164b277502fe44d
drg5567 <drg5567@rit.edu>
2025-03-25 15:55:19 -0400

    Created barebones menu

--- HabitTracker/User.cs
+++ HabitTracker/User.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HabitTracker
 {
     class User
     {
+        private Database database;
+
+        public User(string dbName)
+        {
+            this.database = new Database(dbName);
+        }
+
+        public void DbSession()
+        {
+            Console.WriteLine("Welcome to the habit tracker!");
+            string menuStr = "Menu options:\n" +
+                               "C: create a new habit to log\n" +
+                               "I: insert new record for a given habit" +
+                               "U: update a record for a given habit" +
+                               "D: delete a given habit record" +
+                               "V: view records for a given habit" +
+                               "E: exit";
+            Console.WriteLine(menuStr);
+            var done = false;
+            while (!done)
+            {
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "C":
+                        break;
+                    case "I":
+                        break;
+                    case "U":
+                        break;
+                    case "D":
+                        break;
+                    case "V":
+                        break;
+                    case "E":
+                        done = true;
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid menu choice: try again\n");
+                        break;
+                }
+            }
+        }
     }
 }
