7c0b136e52ac7f9d88635aadd7b0d48a44dac107
drg5567 <drg5567@rit.edu>
2025-03-25 16:28:54 -0400

    Added dbname functionality

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -8,11 +8,26 @@ namespace HabitTracker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter database name:");
-            var dbName = Console.ReadLine();
-            if (dbName == null )
+            Console.WriteLine("Enter database name (must end with '.db'):");
+            var dbName = "habit.db";
+            var valid = false;
+            while (!valid)
             {
-                dbName = "habit.db";
+                var name = Console.ReadLine();
+                if (name == "" || name == null)
+                {
+                    Console.WriteLine($"Resulting to default name '{dbName}'");
+                    valid = true;
+                }
+                else if (name.Length < 4 || name.Substring(name.Length - 3) != ".db")
+                {
+                    Console.WriteLine("Invalid database name");
+                }
+                else
+                {
+                    dbName = name;
+                    valid = true;
+                }
             }
             User user = new User(dbName);
             user.DbSession();
